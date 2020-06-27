f3913994157e7938f2c085d32f3f73d9f212c0f8
Danny <danny.j.eich@gmail.com>
2020-06-27 10:57:54 -0400

    Added 'Update Card Name' to Wallet page

--- src/main/java/com/revature/creditcardrewardtracker/web/CreditCardService.java
+++ src/main/java/com/revature/creditcardrewardtracker/web/CreditCardService.java
@@ -1,13 +1,12 @@
 package com.revature.creditcardrewardtracker.web;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,121 +16,62 @@ import javax.ws.rs.core.Response;
 import com.revature.creditcardrewardtracker.dao.CreditCardRepoDB;
 import com.revature.creditcardrewardtracker.dao.ICreditCardRepo;
 import com.revature.creditcardrewardtracker.models.CreditCard;
-import com.revature.creditcardrewardtracker.models.CreditCardReward;
-import com.revature.creditcardrewardtracker.service.InputValidationService;
 import com.revature.creditcardrewardtracker.service.ValidationService;
 
 @Path("/CreditCardService/{username}")
 public class CreditCardService {
-	
+
 	private ICreditCardRepo d;
 	private ValidationService validation;
-	
+
 	public CreditCardService() {
 		d = new CreditCardRepoDB();
 		validation = new ValidationService();
 	}
-	
-	/*
-	@POST
-	@Path("/new")
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public Response addCreditCard(@PathParam("username") String username, 
-			@FormParam("cardname") String cardName,
-			@FormParam("category") String category,
-			@FormParam("categoryrate") double rate) {
-		//build POJO to add to DB
-		CreditCard card = new CreditCard();
-		card.setCreditCardName(cardName);
-		List<CreditCardReward> rewards = new ArrayList<CreditCardReward>();
-		CreditCardReward reward = new CreditCardReward();
-		reward.setCategoryOfCashBack(category);
-		reward.setPercentageOfCashBack(rate);
-		rewards.add(reward);
-		card.setCardCashBackCategories(rewards);
-		//add POJO to DB
-		d.addCreditCard(username, card);
-		//returns http response
-		return Response.status(201).build();
-	}
-	*/
+
 	@POST
 	@Path("/new")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response addCreditCard(@PathParam("username") String username, 
-			CreditCard card) {
+	public Response addCreditCard(@PathParam("username") String username, CreditCard card) {
 		d.addCreditCard(username, card);
-		//returns http response
+		// returns http response
 		return Response.status(201).build();
 	}
-	
-	
+
 	@GET
 	@Path("/all")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getCreditCards(@PathParam("username") String username) {
-		return Response.ok((ArrayList<CreditCard>)d.getCreditCards(username)).build();
+		return Response.ok((ArrayList<CreditCard>) d.getCreditCards(username)).build();
 	}
-	
+
 	@DELETE
 	@Path("/remove")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response removeCreditCard(@PathParam("username") String username,
-			int creditCardID) {
+	public Response removeCreditCard(@PathParam("username") String username, int creditCardID) {
 		if (validation.permissionToModifyCard(username, creditCardID)) {
 			d.deleteCard(creditCardID);
 			return Response.status(201).build();
 		}
 		return Response.status(401).build();
 	}
-	
-	/*
-	public void deleteCreditCard() {
-		System.out.println("Please enter the Card ID for the credit card you wish to delete.");
-		
-		List<CreditCard> cards = d.getCreditCards(username);
-		for (CreditCard c : cards) {
-			System.out.println(c.stringNameAndId());
-		}
-		
-		int id = inputValidation.getValidInt();
-		
-		boolean belongsToUser = validation.permissionToModifyCard(username, id);
-		
-		if (belongsToUser == true) {
-			boolean result = d.deleteCard(id);
-			if (result == true) {
-				System.out.println(id + " deleted successfully.");
-			} else {
-				System.out.println(id + " not deleted.");
-			}
-		}
-	}
-	
-	public void updateCardName() {
-		System.out.println("Please enter the Card ID for the credit card you wish to modify.");
-		
-		List<CreditCard> cards = d.getCreditCards(username);
-		for (CreditCard c : cards) {
-			System.out.println(c.stringNameAndId());
-		}
-		
-		int id = inputValidation.getValidInt();
-		
-		boolean belongsToUser = validation.permissionToModifyCard(username, id);
-		
-		if (belongsToUser == true) {
-			System.out.println("What would you like to rename this card?");
-			String newName = inputValidation.getValidStringInput();
-			
+
+	@PUT
+	@Path("/updatename")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateCardName(@PathParam("username") String username, CreditCard card) {
+		int id = card.getCreditCardID();
+		if (validation.permissionToModifyCard(username, id)) {
+			String newName = card.getCreditCardName();
 			boolean result = d.updateCard(id, newName);
 			if (result == true) {
-				System.out.println(id + " updated successfully.");
+				System.out.println(id + " updated successfully by user "+username+".");
+				return Response.status(201).build();
 			} else {
 				System.out.println(id + " not updated.");
-			} 
+			}
 		}
+		return Response.status(401).build();
 	}
-	*/
 
 }
